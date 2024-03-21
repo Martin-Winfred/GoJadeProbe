ddf42edb042bd0a75803e0ce834695cc568df991
Martin-Winfred <blakewinfred@gmail.com>
2024-03-21 02:28:16 +0000

    feature add setting function

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
 	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
 	github.com/shirou/gopsutil/v3 v3.24.2 // indirect
--- pkg/setting/setting.go
+++ pkg/setting/setting.go
@@ -2,12 +2,13 @@ package setting
 
 import (
 	"fmt"
-	"os"
 
-	"gopkg.in/ini.v1"
+	guuid "github.com/google/uuid"
+
+	iniv1 "gopkg.in/ini.v1"
 )
 
-type Config struct {
+type Myconfig struct {
 	Address        string
 	Port           int
 	Password       string
@@ -15,72 +16,57 @@ type Config struct {
 	ReportInterval int
 }
 
-func CheckAndCreateIni(path string) error {
-	_, err := os.Stat(path)
-	if os.IsNotExist(err) {
-		cfg := ini.Empty()
-
-		remote, _ := cfg.NewSection("remote")
-		remote.NewKey("address", "example.com")
-		remote.NewKey("port", "8080")
-
-		credentials, _ := cfg.NewSection("credentials")
-		credentials.NewKey("password", "your_password")
-		credentials.NewKey("uuid", "your_uuid")
-
-		settings, _ := cfg.NewSection("settings")
-		settings.NewKey("report_interval", "60")
-
-		err = cfg.SaveTo(path)
-		if err != nil {
-			return err
-		}
-	} else if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func LoadConfig(path string) (*Config, error) {
-	cfg, err := ini.Load(path)
+func LoadIni() (*Myconfig, error) {
+	cfg, err := iniv1.Load("./config.ini")
 	if err != nil {
-		return nil, fmt.Errorf("fail to read file: %v", err)
+		return nil, fmt.Errorf("failed to load config file: %v", err)
 	}
 
-	config := &Config{
-		Address:        cfg.Section("remote").Key("address").String(),
-		Port:           cfg.Section("remote").Key("port").MustInt(),
-		Password:       cfg.Section("credentials").Key("password").String(),
-		UUID:           cfg.Section("credentials").Key("uuid").String(),
-		ReportInterval: cfg.Section("settings").Key("report_interval").MustInt(),
+	sec := cfg.Section("server")
+	if sec == nil {
+		return nil, fmt.Errorf("missing 'server' section in config file")
+	}
+
+	address := sec.Key("address").String()
+	port, err := sec.Key("port").Int()
+	if err != nil {
+		return nil, fmt.Errorf("invalid 'port' value in config file: %v", err)
+	}
+
+	sec = cfg.Section("auth")
+	if sec == nil {
+		return nil, fmt.Errorf("missing 'auth' section in config file")
+	}
+
+	password := sec.Key("password").String()
+
+	uuid := sec.Key("uuid").String()
+	if uuid == "" {
+		uuid = guuid.New().String()
+		sec.Key("uuid").SetValue(uuid)
+		err = cfg.SaveTo("./config.ini")
+		if err != nil {
+			return nil, fmt.Errorf("failed to write UUID to config file: %v", err)
+		}
+	}
+
+	sec = cfg.Section("report")
+	if sec == nil {
+		return nil, fmt.Errorf("missing 'report' section in config file")
+	}
+
+	reportInterval, err := sec.Key("interval").Int()
+	if err != nil {
+		return nil, fmt.Errorf("invalid 'interval' value in config file: %v", err)
+	}
+
+	config := &Myconfig{
+		Address:        address,
+		Port:           port,
+		Password:       password,
+		UUID:           uuid,
+		ReportInterval: reportInterval,
 	}
 
 	return config, nil
-}
-
-func SaveConfig(path string, config *Config) error {
-	cfg := ini.Empty()
-
-	remoteSection, err := cfg.NewSection("remote")
-	if err != nil {
-		return err
-	}
-	remoteSection.NewKey("address", config.Address)
-	remoteSection.NewKey("port", fmt.Sprint(config.Port))
-
-	credentialsSection, err := cfg.NewSection("credentials")
-	if err != nil {
-		return err
-	}
-	credentialsSection.NewKey("password", config.Password)
-	credentialsSection.NewKey("uuid", config.UUID)
-
-	settingsSection, err := cfg.NewSection("settings")
-	if err != nil {
-		return err
-	}
-	settingsSection.NewKey("report_interval", fmt.Sprint(config.ReportInterval))
-
-	return cfg.SaveTo(path)
 }
